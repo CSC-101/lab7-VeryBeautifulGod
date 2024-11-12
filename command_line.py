f6e10b896857ba63848a2fca57e1c31a4db598e0
hailu <hyu37@calpoly.edu>
2024-11-12 03:23:30 -0800

    first commit

--- command_line.py
+++ command_line.py
@@ -1,1 +1,21 @@
+import sys
+from typing import Optional
+
+def str_to_float(s: str) -> Optional[float]:
+    try:
+        return float(s)
+    except ValueError:
+        return None
+
+def sum_command_line_arguments() -> float:
+    total = 0.0
+    for arg in sys.argv[1:]:
+        number = str_to_float(arg)
+        if number is not None:
+            total += number
+    return total
+
+if __name__ == '__main__':
+    total_sum = sum_command_line_arguments()
+    print("Sum of command-line arguments:", total_sum)
 
